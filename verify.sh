9cb8de155a3519c8a2da511bbb019f75bb567994
Linard Arquint <linard.arquint@inf.ethz.ch>
2022-08-11 16:56:05 +0200

    another attempt at fixing stats file upload

--- verify.sh
+++ verify.sh
@@ -7,13 +7,17 @@ scriptDir=$(dirname "$0")
 # flag whether this script is currently executed as part of a CI
 isCi=$CI
 
+# create .gobra folder if it does not exist yet:
+mkdir -p $scriptDir/.gobra
+
 gobraJar="/gobra/gobra.jar"
 additionalGobraArgs="--module gitlab.inf.ethz.ch/arquintl/prototrace --gobraDirectory $scriptDir/.gobra"
+packages="arbitrary"
 
 if [ $isCi ]; then
     echo -e "\033[0Ksection_start:`date +%s`:verify[collapsed=true]\r\033[0KVerifying packages"
 fi
-java -Xss128m -jar $gobraJar --recursive -I $scriptDir $additionalGobraArgs
+java -Xss128m -jar $gobraJar --recursive --includePackages $packages -I $scriptDir $additionalGobraArgs
 exitCode=$?
 if [ $isCi ]; then
     echo -e "\033[0Ksection_end:`date +%s`:verify\r\033[0K"
