db4b299166ca755c00558a71519e85584bc7f22d
Linard Arquint <linard.arquint@inf.ethz.ch>
2022-10-06 13:29:30 +0200

    switches to a pure function to pass properties about LabelingContext to member methods

--- local-verify.sh
+++ local-verify.sh
@@ -9,12 +9,12 @@ isCi=$CI
 
 # gobraJar="/gobra/gobra.jar"
 gobraJar="/Users/arquintlinard/ETH/PhD/gobra/target/scala-2.13/gobra.jar"
-additionalGobraArgs="--module github.com/ModularVerification/ReusableVerificationLibrary --z3Exe /Users/arquintlinard/Downloads/z3-4.8.7-x64-osx-10.14.6/bin/z3"
+additionalGobraArgs="--module github.com/ModularVerification/ReusableVerificationLibrary --z3Exe /Users/arquintlinard/Downloads/z3-4.8.7-x64-osx-10.14.6/bin/z3 --disableGlobalVars"
 
 if [ $isCi ]; then
     echo -e "\033[0Ksection_start:`date +%s`:verify[collapsed=true]\r\033[0KVerifying packages"
 fi
-java -Xss128m -jar $gobraJar --recursive -I $scriptDir $additionalGobraArgs # --printVpr --includePackages tracemanager # --unparse --parseOnly
+java -Xss128m -jar $gobraJar --recursive -I $scriptDir $additionalGobraArgs --includePackages labeledlibrary # --printVpr --includePackages tracemanager # --unparse --parseOnly
 exitCode=$?
 if [ $isCi ]; then
     echo -e "\033[0Ksection_end:`date +%s`:verify\r\033[0K"
