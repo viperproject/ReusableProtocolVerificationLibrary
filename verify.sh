afb3dc6b747383584473432eabdb1abac07fd8a0
Linard Arquint <linard.arquint@inf.ethz.ch>
2022-10-06 13:35:54 +0200

    adds '.verification' as include path

--- verify.sh
+++ verify.sh
@@ -11,7 +11,7 @@ isCi=$CI
 mkdir -p $scriptDir/.gobra
 
 gobraJar="/gobra/gobra.jar"
-additionalGobraArgs="--module github.com/ModularVerification/ReusableVerificationLibrary --gobraDirectory $scriptDir/.gobra"
+additionalGobraArgs="--module github.com/ModularVerification/ReusableVerificationLibrary --include .verification --gobraDirectory $scriptDir/.gobra"
 
 if [ $isCi ]; then
     echo -e "\033[0Ksection_start:`date +%s`:verify[collapsed=true]\r\033[0KVerifying packages"
