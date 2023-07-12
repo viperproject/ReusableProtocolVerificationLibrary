0e65d25727310c1391a7b65d276a705a10843c22
Linard Arquint <linard.arquint@inf.ethz.ch>
2023-07-12 15:49:58 +0200

    Fixes merge error

--- verify.sh
+++ verify.sh
@@ -10,7 +10,6 @@ isCi=$CI
 # create .gobra folder if it does not exist yet:
 mkdir -p $scriptDir/.gobra
 
-<<<<<<< HEAD
 gobraJar="/gobra/gobra.jar"
 additionalGobraArgs="--module github.com/ModularVerification/ReusableVerificationLibrary --include .verification --gobraDirectory $scriptDir/.gobra --parallelizeBranches"
 
@@ -18,23 +17,6 @@ if [ $isCi ]; then
     echo -e "\033[0Ksection_start:`date +%s`:verify[collapsed=true]\r\033[0KVerifying packages"
 fi
 java -Xss128m -jar $gobraJar --recursive -I $scriptDir $additionalGobraArgs
-=======
-preArgs="/usr/local/Cellar/openjdk/19.0.2/bin/java"
-gobraJar="/Users/hugoqueinnec/Documents/ETH/Thesis/gobra/target/scala-2.13/gobra.jar"
-additionalGobraArgs="--module github.com/ModularVerification/ReusableVerificationLibrary --include .verification --gobraDirectory $scriptDir/.gobra --parallelizeBranches"
-
-packageName=$1
-# If a packageName is provided as first argument, only this package is verified:
-if [ -n "$packageName" ]; then
-    echo "Verifying package $packageName"
-    additionalGobraArgs="$additionalGobraArgs --includePackages $packageName"
-fi
-
-if [ $isCi ]; then
-    echo -e "\033[0Ksection_start:`date +%s`:verify[collapsed=true]\r\033[0KVerifying packages"
-fi
-$preArgs -Xss128m -jar $gobraJar --recursive -I $scriptDir $additionalGobraArgs
->>>>>>> 715ef063c5c224d156bb0f96c681d547154b1149
 exitCode=$?
 if [ $isCi ]; then
     echo -e "\033[0Ksection_end:`date +%s`:verify\r\033[0K"
